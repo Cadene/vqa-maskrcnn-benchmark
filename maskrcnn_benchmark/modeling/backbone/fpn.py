1249b4a3d9fcd3ff8f33321b3fe2768667fb77a0
meetshah1995 <meetshah1995@gmail.com>
2018-12-21 15:17:36 -0800

    Modify FPN upsample to support arbitrary shaped lateral features

--- maskrcnn_benchmark/modeling/backbone/fpn.py
+++ maskrcnn_benchmark/modeling/backbone/fpn.py
@@ -54,11 +54,10 @@ class FPN(nn.Module):
         for feature, inner_block, layer_block in zip(
             x[:-1][::-1], self.inner_blocks[:-1][::-1], self.layer_blocks[:-1][::-1]
         ):
-            inner_top_down = F.interpolate(last_inner, scale_factor=2, mode="nearest")
             inner_lateral = getattr(self, inner_block)(feature)
-            # TODO use size instead of scale to make it robust to different sizes
-            # inner_top_down = F.upsample(last_inner, size=inner_lateral.shape[-2:],
-            # mode='bilinear', align_corners=False)
+            inner_top_down = F.interpolate(
+                last_inner, size=inner_lateral.shape[-2:], mode="nearest"
+            )
             last_inner = inner_lateral + inner_top_down
             results.insert(0, getattr(self, layer_block)(last_inner))
 
